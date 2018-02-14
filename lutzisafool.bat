ca31f35cb24f635e4bc85a6cb4e1d39930a830b7
Nick Lutz <nicklutz89@gmail.com>
2018-02-14 18:17:04 -0500

    Batch file to run the site.

---
+++ lutzisafool.bat
@@ -0,0 +1,11 @@
+cd c:\
+mkdir lutzdevinterview
+cd lutzdevinterview
+python -m venv lutzdev
+pip install django~=1.11.0
+git init
+git remote add origin https://github.com/lutzitania/fool
+git fetch origin
+git reset --hard origin/master
+python manage.py migrate
+python manage.py runserver
